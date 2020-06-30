d92752d465e02eb30e2aff48e492386cc7eb1f4a
darshanadsw <darshanadsw@gmail.com>
2020-06-30 09:14:11 -0400

    Initial commit

---
+++ src/test/java/dw/springframework/oauthsecurity/OauthSecurityApplicationTests.java
@@ -0,0 +1,13 @@
+package dw.springframework.oauthsecurity;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OauthSecurityApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
